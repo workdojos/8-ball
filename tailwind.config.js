5fa993a354a621f49f0e6cf2879d75096ff4abf0
WorkDojos <36578874+workdojos@users.noreply.github.com>
2024-12-06 12:44:53 -0600

    d

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        gray: {
-          200: "#D5DAE1"
-        },
-        black: {
-          DEFAULT: "#000",
-          500: "#1D2235"
-        },
-        blue: {
-          500: "#2b77e7"
-        }
+      fontFamily:{
+        inter: ['var(--font-inter)']
       },
-      fontFamily: {
-        worksans: ["Work Sans", "sans-serif"],
-        poppins: ['Poppins', "sans-serif"]
+      colors:{
+        background: 'rgb(var(--background))',
+        foreground: 'rgb(var(--foreground))',
+        muted: 'rgb(var(--muted))',
+        accent: 'rgb(var(--accent))',
       },
-      boxShadow: {
-        card: '0px 1px 2px 0px rgba(0, 0, 0, 0.05)'
+      backgroundImage:{
+        'firefly-radial': "radial-gradient(50% 50% at 50% 50%, rgba(253, 255, 80, 0.5) 0%, rgba(217,217,217, 0) 100%)"
+      },
+      boxShadow:{
+        'glass-inset': 'inset 0 17px 5px -9px rgba(254,254,91, 0.05)',
+        'glass-sm': '5px 5px 20px 0px rgba(254,254,91, 0.3)',
+      },
+      keyframes:{
+      'spin-reverse':{
+        '0%': {transform: 'rotate(0deg)'},
+        '100%': {transform: 'rotate(-360deg)'}
+      }
+      },
+      animation:{
+        'spin-slow': 'spin 40s linear infinite',
+        'spin-slow-reverse': 'spin-reverse 40s linear infinite',
+      },
+      screens:{
+        xs: '480px',
       }
     },
   },
   plugins: [],
-}
-
+};
