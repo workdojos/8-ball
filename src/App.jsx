7dc8d53b30d93dc4a05ff7205223925ce1711580
WorkDojos <36578874+workdojos@users.noreply.github.com>
2024-12-06 12:29:52 -0600

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,25 @@
-// Beachy Beachy Ball
-// Copyright (c) 2023 Michael Kolesidis <michael.kolesidis@gmail.com>
-// Licensed under the GNU Affero General Public License v3.0.
-// https://www.gnu.org/licenses/gpl-3.0.html
+import Hero from './sections/Hero.jsx';
+import About from './sections/About.jsx';
+import Footer from './sections/Footer.jsx';
+import Navbar from './sections/Navbar.jsx';
+import Contact from './sections/Contact.jsx';
+import Clients from './sections/Clients.jsx';
+import Projects from './sections/Projects.jsx';
+import WorkExperience from './sections/Experience.jsx';
 
-import { Canvas } from "@react-three/fiber";
-// import { EffectComposer, Pixelation } from "@react-three/postprocessing";
-import useGame from "./stores/useGame.js";
-import Game from "./Game.jsx";
-import Interface from "./interface/Interface";
-import Controls from "./utils/Controls";
-import MainMenu from "./interface/MainMenu.jsx";
-
-// Prevent right click
-document.addEventListener("contextmenu", (e) => e.preventDefault());
-
-export default function App() {
-  const isInGame = useGame((state) => state.isInGame);
-  // const pixalated = useGame((state) => state.pixalated);
-
+const App = () => {
   return (
-    <>
-      {isInGame ? (
-        <Controls>
-          <Canvas
-            shadows
-            camera={{
-              fov: 45,
-              near: 0.1,
-              far: 200,
-              position: [2.5, 4, 6],
-            }}
-          >
-            {/* {pixalated && (
-              <EffectComposer>
-                <Pixelation granularity={5} />
-              </EffectComposer>
-            )} */}
-            <Game />
-          </Canvas>
-          <Interface />
-        </Controls>
-      ) : (
-        <MainMenu />
-      )}
-    </>
+    <main className="max-w-7xl mx-auto relative">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Clients />
+      <WorkExperience />
+      <Contact />
+      <Footer />
+    </main>
   );
-}
+};
+
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const isCodeSandbox =
-  "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
-
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [react()],
-  root: "src/",
-  publicDir: "../public/",
-  base: "./",
-  server: {
-    host: true,
-    open: !isCodeSandbox, // Open if it's not a CodeSandbox
-  },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    sourcemap: true,
-    minify: "esbuild",
-    cssMinify: true,
-  },
-};
+})
