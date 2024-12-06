8c0affc7d1a64f4931516dd770038695c42f3ae8
WorkDojos <36578874+workdojos@users.noreply.github.com>
2024-12-06 11:28:57 -0600

    sd

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import EightBall from "./EightBall";
+
+/** Component for entire page.
+ *
+ * Props: none
+ * State: none
+ * App -> EightBall
+ *
+*/
+
+function App() {
+
+  return (
+    <div className="App">
+      <EightBall/>
+    </div>
+  )
+}
+
+export default App;
